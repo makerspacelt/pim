83baff47038713c6da196d3415c79f5cfe3a7940
kulverstukas1 <kulverstukas.k@gmail.com>
2018-05-09 23:01:01 +0300

    maži pakeitimai

--- couch/snippets/head.php
+++ couch/snippets/head.php
@@ -5,8 +5,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
         <meta name="author" content="">
-
-        <title><cms:get_custom_field var='site_title' masterpage='index.php' /></title>
+        
+        <cms:capture into='cur_site_title'>
+            <cms:get_custom_field var='site_title' masterpage='index.php' />
+        </cms:capture>
+        <cms:if "<cms:not_empty cur_site_title />">
+            <title><cms:show cur_site_title /></title>
+        </cms:if>
 
         <!-- Bootstrap core CSS -->
         <link href="<cms:show k_site_link />vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -15,8 +20,13 @@
         <link href="<cms:show k_site_link />css/main.css" rel="stylesheet">
         
         <!-- Favikonos rodymas -->
-        <link rel="shortcut icon" href="<cms:get_custom_field var='site_favicon' masterpage='index.php' />" type="image/x-icon">
-        <link rel="icon" href="<cms:get_custom_field var='site_favicon' masterpage='index.php' />" type="image/x-icon">
+        <cms:capture into='cur_site_favicon'>
+            <cms:get_custom_field var='site_favicon' masterpage='index.php' />
+        </cms:capture>
+        <cms:if "<cms:not_empty cur_site_favicon />">
+            <link rel="shortcut icon" href="<cms:show cur_site_favicon />" type="image/x-icon">
+            <link rel="icon" href="<cms:show cur_site_favicon />" type="image/x-icon">
+        </cms:if>
     </head>
 
     <body>
--- couch/snippets/header.php
+++ couch/snippets/header.php
@@ -1,7 +1,20 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
   <div class="container">
     <a href="<cms:show k_site_link />">
-        <img src="<cms:get_custom_field var='site_logo' masterpage='index.php' />" class="site-logo" /><div class="navbar-brand"><cms:get_custom_field var='site_title' masterpage='index.php' /></div>
+        <cms:capture into='cur_site_logo'>
+            <cms:get_custom_field var='site_logo' masterpage='index.php' />
+        </cms:capture>
+        <cms:if "<cms:not_empty cur_site_logo />">
+            <img src="<cms:show cur_site_logo />" class="site-logo" />
+        </cms:if>
+        
+        <cms:capture into='cur_site_title'>
+            <cms:get_custom_field var='site_title' masterpage='index.php' />
+        </cms:capture>
+        <cms:if "<cms:not_empty cur_site_title />">
+            <div class="navbar-brand"><cms:show cur_site_title /></div>
+        </cms:if>
+        
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
