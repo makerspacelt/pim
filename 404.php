e93f85c14c7339467cd06657acd8cb62c211bfcc
kulverstukas1 <kulverstukas.k@gmail.com>
2018-05-11 08:25:18 +0300

    pridėtas custom 404 puslapis

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php require_once( 'couch/cms.php' ); ?>
+<cms:template name='404' title='Klaida 404' hidden='1'>
+</cms:template>
+
+<cms:embed 'head.php' />
+<cms:embed 'header.php' />
+<div class="container">
+    <div class="row">
+        <cms:embed 'sidebar.php' />
+        <div class="col-lg-9">
+            <img class="img-fluid" src="images/404-img.jpg">
+        </div>
+    </div>
+</div>
+<cms:embed 'foot.php' />
+<?php COUCH::invoke(); ?>
