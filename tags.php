ba9d6cefc940c3b8ff3247624cc1c39aae218f95
kulverstukas1 <kulverstukas.k@gmail.com>
2018-05-14 17:10:18 +0300

    pakeitimai ivairus

--- tags.php
+++ tags.php
@@ -6,4 +6,19 @@
         <cms:field 'k_actions' />
     </cms:config_list_view>
 </cms:template>
+
+<cms:embed 'head.php' />
+<cms:embed 'header.php' />
+<div class="container">
+    <div class="row">
+        <cms:embed 'sidebar.php' />
+        
+        <div class="col-lg-9">
+            <div class="row my-4">
+                sasasa
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php COUCH::invoke(); ?>
