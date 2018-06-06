68e3c8284a2e52a8d954aabd8043c9f6c9425ebc
Kulverstukas <kulverstukas.k@gmail.com>
2018-06-06 17:50:32 +0300

    padarytas unikalaus kodo generavimas; TODO: kodą turi generuoti tik iš admin panelės, kuriant naują įrankį

--- couch/addons/code-generator/code-generator.php
+++ couch/addons/code-generator/code-generator.php
@@ -1,27 +1,43 @@
 <?php
 /*
     Addon'as kuris sugeneruoja naujà unikalø kodà árankiui
+    TODO: kodà turi generuoti tik kuriant naujà áranká admin panelëje,
+    dabar já generuoja (bet niekur neiðveda) kai ateini á puslapá taip pat.
 */
 
 if (!defined('K_COUCH_DIR')) die(); // cannot be loaded directly
 
 class code_generator_class {
     
-    function generate($params) {
-        global $DB, $TAGS, $PAGE;
-        // echo '<pre>'; print_r($this->page); die();
-        // if( $PAGE->page_name != '' ) return 'qwqwq';
+    private function generateCode() {
+        $code = '';
+        for ($i = 0; $i < 11; $i++) {
+            $code .= strval(rand(0, 9));
+        }
+        return $code;
+    }
+    
+    public function generate($params) {
+        global $DB;
+        
         if ((count($params) > 0) && ($params[0]['lhs'] == 'field_name') && ($params[0]['rhs'] != '')) {
-            $rs = $DB->raw_select(sprintf('SELECT %1$s.value, %2$s.name FROM %1$s RIGHT JOIN %2$s ON %2$s.id = %1$s.field_id WHERE %2$s.name = \'%3$s\' AND %1$s.value != \'\' AND %1$s.value = \'%4$s\'',
-            K_TBL_DATA_TEXT, K_TBL_FIELDS, $params[0]['rhs'], '6432154698'));
-            // echo '<pre>'; print_r($rs); die();
-            return 'asasasasasasa';
+            $exists = true;
+            $tries = 0;
+            while ($exists) {
+                $tries++;
+                $rs = $DB->raw_select(sprintf('SELECT %1$s.value, %2$s.name FROM %1$s RIGHT JOIN %2$s ON %2$s.id = %1$s.field_id WHERE %2$s.name = \'%3$s\' AND %1$s.value != \'\' AND %1$s.value = \'%4$s\'',
+                K_TBL_DATA_TEXT, K_TBL_FIELDS, $params[0]['rhs'], $code));
+                $code = $this->generateCode();
+                if (count($rs) == 0) $exists = false;
+            }
+            // echo $tries;
+            return $code;
         } else {
-            return 'zzzzz';
+            return '';
         }
     }
 
 }
-
-$FUNCS->register_tag('generate-code', array('code_generator_class', 'generate'));
+$codeGen = new code_generator_class();
+$FUNCS->register_tag('generate-code', array($codeGen, 'generate'));
 ?>
