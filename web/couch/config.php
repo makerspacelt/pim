4e768f641a58439d44c3ecdeaad8fe831ec2c701
Darius Vozbutas <darius.vozbutas@nfq.lt>
2018-06-18 11:32:57 +0000

    selecting correct timezone (for now)

--- web/couch/config.php
+++ web/couch/config.php
@@ -30,7 +30,7 @@
     // -4.5   Venezuela (UTC-04:30)
     // -6     United States (Central Time) (UTC-06:00)
     // -8     United States (Pacific Time) (UTC-08:00)
-    define( 'K_GMT_OFFSET', +5.5 );
+    define( 'K_GMT_OFFSET', +3 );
 
     // 3.
     // Define the charset used by your site. If in any doubt, leave the default utf-8.
